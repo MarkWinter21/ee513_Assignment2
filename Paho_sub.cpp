7872d196d6a884c5b03be90720a531fac40d2b0d
MarkWinter21 <marklyonsxyz@hotmail.com>
2022-03-21 16:46:46 +0000

    first commit

---
+++ Paho_sub.cpp
@@ -0,0 +1,145 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include "MQTTClient.h"
+#include <wiringPi.h>
+#include <unistd.h>
+
+#define ADDRESS "tcp://192.168.1.97:1883"
+#define CLIENTID "rpi2"
+//#define CLIENTID "rpi3" // uncomment for second subscriber
+#define AUTHMETHOD "Hooke915!"
+#define AUTHTOKEN "Hooke915!"
+#define TOPIC "ee513/CPUTemp"
+#define PAYLOAD "Hello World!"
+#define QOS 1
+#define TIMEOUT 10000L
+
+#define GPIO_ROLL 0   //this is the roll GPIO
+#define GPIO_PITCH 2  //this is the pitch GPIO 
+
+using namespace std;
+char buffer[1000];
+float float_array [10];
+
+volatile MQTTClient_deliveryToken deliveredtoken;
+
+void delivered(void *context, MQTTClient_deliveryToken dt) {
+printf("Message with token value %d delivery confirmed\n", dt);
+deliveredtoken = dt;
+}
+
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
+int i;
+char* payloadptr;
+printf("Message arrived\n");
+printf("topic: %s\n", topicName);
+printf("message: ");
+payloadptr = (char*) message->payload;
+//for(i=0; i<2; i++) {
+//putchar(*payloadptr++);
+//char buffer [1000];
+printf("The payload: %s",payloadptr);
+sprintf(buffer, "individual: %s",payloadptr);
+//}
+
+int n = sizeof(buffer);
+char char_array[n+1];
+int a;
+string ax ="the 23.3  number 444";
+//getNumberFromString (buffer);
+stringstream str_strm;
+str_strm.precision(2);
+str_strm << buffer;
+string temp_str;
+float temp_int;
+int z=0;
+for (int d =0; d<sizeof(buffer); d++){
+
+str_strm>> temp_str; //take wordsinto temp_str one by one
+if(stringstream(temp_str)>>temp_int){//try to convert string to int
+cout<<temp_int<<" "<<endl;
+float_array[z]=temp_int;
+z++;
+}
+temp_str = " "; //clear temp string
+}
+
+if (n>20){
+for (a=35; a<38; a++){
+char_array[a-35]=buffer[a];}
+printf("The output: %s",char_array);}
+putchar('\n');
+printf("%f",float_array[2]);
+if (float_array[1]>10){
+digitalWrite(GPIO_ROLL, HIGH);}
+else {digitalWrite(GPIO_ROLL, LOW);}
+//if(float_array[2]>10){
+//digitalWrite(GPIO_PITCH, HIGH);}
+//else {digitalWrite(GPIO_PITCH, LOW);}
+
+MQTTClient_freeMessage(&message);
+MQTTClient_free(topicName);
+return 1;
+}
+
+void connlost(void *context, char *cause) {
+printf("\nConnection lost\n");
+printf("cause: %s\n", cause);
+}
+
+void getNumberFromString (std::string s) {
+stringstream str_strm;
+str_strm<< s; //convert sting into stringstream
+string temp_str;
+int temp_int;
+while (str_strm.eof() != 1){
+str_strm >> temp_str;//take words into temp str one by one
+if(stringstream(temp_str) >> temp_int){ //try to convert string to int
+cout <<temp_int<<" ";
+}
+temp_str = " "; //clear temp string
+}
+}
+
+int main(int argc, char* argv[]) {
+//MQTTClient client;
+
+wiringPiSetup();
+pinMode(GPIO_PITCH, OUTPUT);
+pinMode(GPIO_ROLL, OUTPUT);
+digitalWrite(GPIO_ROLL, LOW);
+digitalWrite(GPIO_PITCH, LOW);
+
+//usleep(1000000);
+
+//digitalWrite(GPIO_ROLL, HIGH);
+//digitalWrite(GPIO_PITCH, HIGH);
+
+MQTTClient client;
+MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
+int rc;
+int ch;
+MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_DEFAULT, NULL);
+opts.keepAliveInterval = 20;
+opts.cleansession = 0;
+opts.username = AUTHMETHOD;
+opts.password = AUTHTOKEN;
+MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
+if ((rc = MQTTClient_connect(client, &opts)) != MQTTCLIENT_SUCCESS) {
+printf("Failed to connect, return code %d\n", rc);
+exit(-1);
+}
+printf("Subscribing to topic %s\nfor client %s using QoS%d\n\n"
+"Press Q<Enter> to quit\n\n", TOPIC, CLIENTID, QOS);
+MQTTClient_subscribe(client, TOPIC, QOS);
+do {
+ch = getchar();
+} while(ch!='Q' && ch != 'q');
+MQTTClient_disconnect(client, 10000);
+MQTTClient_destroy(&client);
+return rc;
+}
---
+++ application.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include "ADXL345.h"
+#include <unistd.h>
+#include <pthread.h>
+#include "MQTTClient.h"
+#define CPU_TEMP "/sys/class/thermal/thermal_zone0/temp"
+
+using namespace std;
+using namespace exploringRPi;
+
+//Please replace the following address with the address of your server
+#define ADDRESS "tcp://192.168.8.103:1883"
+#define CLIENTID "rpi1"
+#define AUTHMETHOD "Hooke915!"
+#define AUTHTOKEN "Hooke915!"
+#define TOPIC "ee513/CPUTemp"
+#define QOS 2
+#define TIMEOUT 10000L
+
+float getCPUTemperature() {
+// get the CPU temperature
+int cpuTemp;
+// store as an int 
+fstream fs;
+fs.open(CPU_TEMP, fstream::in); // read from the file 
+fs >> cpuTemp;
+fs.close();
+return (((float)cpuTemp)/1000);
+}
+
+int main(int argc, char* argv[]) {
+char str_payload[100];
+
+struct timeval begin, end;
+gettimeofday(&begin, 0);
+
+
+ADXL345 sensor(1,0x1d);
+sensor.setResolution(ADXL345::NORMAL);
+sensor.setRange(ADXL345::PLUSMINUS_4_G);
+int i=1;
+int rc;
+
+while (i<=10)
+{
+++i;
+
+gettimeofday(&end, 0);
+long seconds = end.tv_sec - begin.tv_sec;
+long microseconds = end.tv_usec - begin.tv_usec;
+double elapsed =seconds + microseconds*1e-6;
+cout<<elapsed<<endl;
+
+sensor.displayPitchAndRoll();
+
+// Set your max message size here
+MQTTClient client;
+MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
+MQTTClient_message pubmsg = MQTTClient_message_initializer;
+MQTTClient_willOptions willOpts = MQTTClient_willOptions_initializer;
+MQTTClient_deliveryToken token;
+MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+
+willOpts.topicName ="ee513/CPUTemp";
+willOpts.message = "Last will triggered";
+willOpts.retained = 0;
+willOpts.qos =1;
+
+opts.keepAliveInterval = 20;
+opts.cleansession = 1;
+opts.username = AUTHMETHOD;
+opts.password = AUTHTOKEN;
+
+//int rc;
+if ((rc = MQTTClient_connect(client, &opts)) != MQTTCLIENT_SUCCESS) {
+cout << "Failed to connect, return code " << rc << endl;
+
+return -1;
+}
+sprintf(str_payload, "{\"d\":{\"CPUTemp\": %.2f, \"Pitch\": %.2f, \"Roll\": %.2f, \"Time\": %.2f}", getCPUTemperature(), sensor.getPitch(), sensor.getRoll(), (double)elapsed);
+pubmsg.payload = str_payload;
+pubmsg.payloadlen = strlen(str_payload);
+pubmsg.qos = 1;
+pubmsg.retained = 0;
+MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+cout << "Waiting for up to " << (int)(TIMEOUT/1000) <<
+" seconds for publication of " << str_payload <<
+" \non topic " << TOPIC << " for ClientID: " << CLIENTID << endl;
+rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+cout << "Message with token " << (int)token << " delivered." << endl;
+MQTTClient_disconnect(client, 10000);
+MQTTClient_destroy(&client);
+//return rc;
+}
+
+return rc;
+}
